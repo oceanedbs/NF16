8bbae9e40f205eb7425bfc20747e59de65012dda
theLoulou <31326331+theLoulou@users.noreply.github.com>
2017-10-23 18:51:03 +0200

    Update main.c

--- TP03/main.c
+++ TP03/main.c
@@ -3,7 +3,16 @@
 #include <string.h>
 #include "TP03.h"
 
-
+/*				FONCTION PRGRM PRINCIPAL -> faire menu interactif (toutes les fonctions sont pretes)
+1. Créer une liste
+2. Ajouter un élément dans une liste
+3. Supprimer un élément d’une liste
+4. Rechercher un élément dans une liste
+5. Afficher les éléments d’une liste
+6. Supprimer une liste
+7. Fusionner deux listes
+8. Quitter
+*/
 int main() {
     char* a="Hey", *c="Oooh";
     T_Element* new_elemt,*new_elemt2, *rechercher;
@@ -15,7 +24,7 @@ int main() {
     new_elemt2 = creerElement(c);
 
     if(new_elemt != NULL){ printf("Element cree \n");}
-    else {printf("Echec cr�ation element \n");}
+    else {printf("Echec création element \n");}
 
     new_lst = creerListe();
     new_lst2 = creerListe();
@@ -68,3 +77,49 @@ int main() {
 
     return 0;
 }
+
+
+/*
+//VRAI MAIN :*
+
+int main() {
+int a,i=0;
+int b;
+char v[20];
+T_Liste* tab[20];
+T_Liste* list;
+for (a=0;a<=20;a++) {tab[a]=NULL;}
+	while(1) {
+	
+		a = menu();
+		if (a==8) {return 0;}
+		if (a==1) {
+			for (i=0;i<=20;i++) {
+				if (tab[i]==NULL) {
+					tab[i] = creerListe();
+					printf("liste cree sous tab[%d] /n",i);
+					break;
+				}
+			}
+		}
+		if (a==2) {
+			printf("veuillez choisir la liste concernee (entrer l'indice du tableau)");
+			if ((scanf("%d",&b) == 1) && (b<=20) && (b>=0)) {
+				if (tab[b]==NULL) {printf("indice incorrect, pas de liste ici /n");}
+				else {
+					list = tab[b];
+					printf("entrez la valeur a inserer /n");
+					scanf("%s",v);
+					i = insererElement(list, v);
+					if (i==0) {printf("insertion reussie /n");}
+					if (i==-1) {printf("insertion failed /n");}
+				}
+			}
+			else {printf("veuillez entrer une valeur correcte /n");}
+		}
+		if (a==3) {}
+	}
+
+}
+
+*/
