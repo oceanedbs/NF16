0380dd5e185f3b815dc1c10a49a5e274c899069d
theLoulou <31326331+theLoulou@users.noreply.github.com>
2017-10-26 15:30:13 +0200

    Update main.c

--- TP03/main.c
+++ TP03/main.c
@@ -24,22 +24,23 @@ pretes)
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define TAILLE 20
 
 int main() {
   int a, i = 0, k;
   T_Element *j;
   int b, c, d = 0;
   char v[20];
-  T_Liste *tab[20];
+  T_Liste *tab[TAILLE];
   T_Liste *list;
-  for (a = 0; a <= 20; a++) {
+  for (a = 0; a <= TAILLE; a++) {
     tab[a] = NULL;
   }
   while (1) {
     a = 0;
     a = menu();
     if (a == 8) {
-      for (d = 0; d <= 20; d++) {
+      for (d = 0; d <= TAILLE; d++) {
         supprimerListe(tab[d]);
       }
       printf("Au revoir !\n");
@@ -47,7 +48,7 @@ int main() {
     }
     if (a == 1) {
       d = 0;
-      for (i = 0; i <= 20; i++) {
+      for (i = 0; i <= TAILLE; i++) {
         if (tab[i] == NULL) {
           tab[i] = creerListe();
           printf("liste cree sous tab[%d] \n", i);
@@ -55,7 +56,7 @@ int main() {
         }
         d++;
       }
-      if (d == 21) {
+      if (d == TAILLE+1) {
         printf("pas assez de place pour mettre d'autres liste, veuillez en "
                "supprimer une ancienne \n");
       }
@@ -66,7 +67,7 @@ int main() {
       if (k == 1) {
         printf("veuillez choisir la liste concernee (entrer l'indice du "
                "tableau) \n");
-        if ((scanf("%d", &b) == 1) && (b <= 20) && (b >= 0)) {
+        if ((scanf("%d", &b) == 1) && (b <= TAILLE) && (b >= 0)) {
           if (tab[b] == NULL) {
             printf("indice incorrect, pas de liste ici \n");
           } else {
@@ -97,7 +98,7 @@ int main() {
 
         printf("veuillez choisir la liste concernee (entrer l'indice du "
                "tableau) \n");
-        if ((scanf("%d", &b) == 1) && (b <= 20) && (b >= 0)) {
+        if ((scanf("%d", &b) == 1) && (b <= TAILLE) && (b >= 0)) {
           if (tab[b] == NULL) {
             printf("indice incorrect, pas de liste ici \n");
           } else {
@@ -129,7 +130,7 @@ int main() {
 
         printf("veuillez choisir la liste concernee (entrer l'indice du "
                "tableau)\n");
-        if ((scanf("%d", &b) == 1) && (b <= 20) && (b >= 0)) {
+        if ((scanf("%d", &b) == 1) && (b <= TAILLE) && (b >= 0)) {
           if (tab[b] == NULL) {
             printf("indice incorrect, pas de liste ici \n");
           } else {
@@ -160,7 +161,7 @@ int main() {
       if (k == 1) {
         printf("veuillez choisir la liste concernee (entrer l'indice du "
                "tableau) \n");
-        if ((scanf("%d", &b) == 1) && (b <= 20) && (b >= 0)) {
+        if ((scanf("%d", &b) == 1) && (b <= TAILLE) && (b >= 0)) {
           if (tab[b] == NULL) {
             printf("indice incorrect, pas de liste ici \n");
           } else {
@@ -188,7 +189,7 @@ int main() {
 
         printf("veuillez choisir la liste concernee (entrer l'indice du "
                "tableau) \n");
-        if ((scanf("%d", &b) == 1) && (b <= 20) && (b >= 0)) {
+        if ((scanf("%d", &b) == 1) && (b <= TAILLE) && (b >= 0)) {
           if (tab[b] == NULL) {
             printf("indice incorrect, pas de liste ici \n");
           } else {
@@ -216,13 +217,13 @@ int main() {
       if (k == 1) {
         printf("veuillez choisir la premiere liste concernee (entrer l'indice "
                "du tableau) \n");
-        if ((scanf("%d", &b) == 1) && (b <= 20) && (b >= 0)) {
+        if ((scanf("%d", &b) == 1) && (b <= TAILLE) && (b >= 0)) {
           if (tab[b] == NULL) {
             printf("indice incorrect, pas de liste ici \n");
           } else {
             printf("veuillez choisir la deuxieme liste concernee (entrer "
                    "l'indice du tableau) \n");
-            if ((scanf("%d", &i) == 1) && (i <= 20) && (i >= 0)) {
+            if ((scanf("%d", &i) == 1) && (i <= TAILLE) && (i >= 0)) {
               if (tab[i] == NULL) {
                 printf("indice incorrect, pas de liste ici \n");
               } else {
