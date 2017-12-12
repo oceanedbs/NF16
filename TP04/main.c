abe99429e4eb8298fa14f201198e6aa998275baf
theLoulou <31326331+theLoulou@users.noreply.github.com>
2017-12-12 11:23:02 +0100

    Update main.c

--- TP04/main.c
+++ TP04/main.c
@@ -4,7 +4,7 @@
 #include "tp4.h"
 
 int main(){
-  Arbre *dico;
+  /*Arbre *dico;
   int w;
   w=strncmp("rapide", "rap", strlen("rap"));
   printf("%d", w);
@@ -26,9 +26,48 @@ int main(){
 
  rechercherMot(dico, "rap");
 
- supprimeMot(dico, "tabouret");
+ suggestionMots(dico, "ra", 5);*/
 
- suggestionMots(dico, "ra", 5);
+ Dico* dico = malloc(sizeof(Dico));
+ Dico* dico2= malloc(sizeof(Dico));
+  Dico* dico3= malloc(sizeof(Dico));
+   Dico* dico4= malloc(sizeof(Dico));
+   Dico* dico5= malloc(sizeof(Dico));
+   Dico* dico6= malloc(sizeof(Dico));
 
+dico->c='m';
+dico->succ=dico2;
+dico->alt=NULL;
+dico2->c='a';
+dico2->succ=dico3;
+dico2->alt=NULL;
+dico3->c='n';
+dico3->succ=dico4;
+dico3->alt=NULL;
+dico4->c='$';
+dico4->succ=NULL;
+dico4->alt=dico5;
+dico5->c='u';
+dico5->succ=dico6;
+dico5->alt=NULL;
+dico6->c='$';
+dico6->succ=NULL;
+dico6->alt=NULL;
+
+Mot* mot= creerMot();
+printf("done \n");
+ printf("resultat  = %d \n",rechercheMot2(dico,mot));
+Dico* doc;
+doc = prefixeMot(dico,mot);
+printf("doc = %c \n",doc->c);
+//dico = supprimeMot2(mot,dico);
+/*printf("appels a print1 puis 2\n");
+char tab[100];
+int u;
+for(u=0;u<100;u++){tab[u]=NULL;}
+tab[0]='m';
+print(dico2,tab,1);
+printf("\n wait bitch \n");*/
+print2(dico,mot);
   return 0;
 }
